64d8b5a53103f1f6ec4fa9d230b7febdef94c020
fruzitent <fruzit@gmail.com>
2024-04-27 20:56:25 +0300

    deps: update dependency whiskers to v2.1.0

---
+++ build.cmd
@@ -0,0 +1,11 @@
+@echo on
+setlocal EnableDelayedExpansion
+
+forfiles /c "cmd /c if @isDir == FALSE pushd %~dp0 && whiskers @path" /m "*.tera" /p ".\src" /s
+
+for /f %%a in ('forfiles /c "cmd /c if @isDir == TRUE echo @path" /p ".\dist"') do (
+  set "_FILES="
+  for /f "delims=" %%b in ('forfiles /c "cmd /c if @isDir == FALSE echo @relpath" /p "%%a" /s') do ( set "b=%%b" && set "_FILES=!_FILES! !b:~3,-1!" )
+  set "FILES=!_FILES:~1!"
+  tar --create --directory "%%a" --file "%%a.tar.gz" --gzip !FILES!
+)
